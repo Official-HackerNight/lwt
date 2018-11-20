60cc23c8c9c66523738a2b7f841d22a7e48ed3fc
Steven K <steven.kelsey@revature.com>
2018-11-20 16:23:47 -0500

    last week tonight jhipster app initialized

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -1,10 +1,16 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
+import { LwtEpisodeModule } from './episode/episode.module';
+import { LwtResourceModule } from './resource/resource.module';
+import { LwtTagModule } from './tag/tag.module';
 /* jhipster-needle-add-entity-module-import - JHipster will add entity modules imports here */
 
 @NgModule({
     // prettier-ignore
     imports: [
+        LwtEpisodeModule,
+        LwtResourceModule,
+        LwtTagModule,
         /* jhipster-needle-add-entity-module - JHipster will add entity modules here */
     ],
     declarations: [],
