2fd0786b4cc12f4bbaa31500f73c36403f9631ba
Steven K <steven.kelsey@revature.com>
2018-11-23 23:39:36 -0500

    created episode component

---
+++ src/main/webapp/app/pages/episode/episode.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'jhi-episode',
+    templateUrl: './episode.component.html',
+    styles: []
+})
+export class EpisodeComponent implements OnInit {
+    constructor() {}
+
+    ngOnInit() {}
+}
